9cd77f81205626a6c02047617ca1be56bcf5278d
Sandas Gimhana <109182524+sandazz@users.noreply.github.com>
2024-02-02 13:28:46 +0530

    up to task 03 done - connected CSS bootstrap - pass style from react without JSX - create git - created index.js and index.html files -  manipulate html using js file

---
+++ task 03/index.js
@@ -0,0 +1,23 @@
+function changeColor(){
+    document.getElementById("textchangeColor").style.color = "red";
+}
+
+function resetColor(){
+    document.getElementById("textchangeColor").style.color = "black";
+}
+
+const changeSize = () => {
+    document.getElementById("textChangeSize").style.fontSize = "20px";
+}
+
+const resetSize = () => {
+    document.getElementById("textChangeSize").style.fontSize = "16px";
+}
+
+const changeToBold = () => {
+    document.getElementById("textChangeToBold").style.fontWeight = "bold";
+}
+
+const resetToBold = () => {
+    document.getElementById("textChangeToBold").style.fontWeight = "normal";
+}
